d3183ce92d07f1082b5ceeb1d224aafec9fec905
Bodya <b.pushkar@mail.ru>
2016-10-20 19:59:37 +0300

    SettingsView

---
+++ Control Your Servers/Controller/SettingsViewController.h
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.h
+//  Control Your Servers
+//
+//  Created by Bodya on 12.10.16.
+//  Copyright © 2016 Aleksey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+
+@end
